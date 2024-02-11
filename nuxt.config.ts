1f0fd67d391450033a6197e0e6cdf9feb300c4ca
ralphlol123 <ralphsakuragi@gmail.com>
2024-02-12 00:45:46 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['nuxt-swiper']
+  devtools: { enabled: false },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    'nuxt-swiper',
+    '@nuxtjs/tailwindcss',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
 })
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,90 @@
-<script setup>
-const slides = ref(Array.from({ length: 5 }, () => {
-  const r = Math.floor(Math.random() * 256)
-  const g = Math.floor(Math.random() * 256)
-  const b = Math.floor(Math.random() * 256)
-  // Figure out contrast color for font
-  const contrast = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? 'black' : 'white'
+<script setup lang="ts">
+ import { Swiper, SwiperSlide } from 'swiper/vue';
+ import { EffectCards } from 'swiper/modules';
 
-  return { bg: `rgb(${r}, ${g}, ${b})`, color: contrast }
-}))
+ const modules = [EffectCards];
+
+
 </script>
 
 <template>
-  <div class="tw-bg-teal-500">
-    <h2 >Swipera asdCard Effect</h2>
+  <div class="tw-bg-red-400">
+    <p class="tw-text-center tw-font-serif tw-font-semibold tw-text-6xl">
+      hellos
+    </p>
+    <div class="tw-flex tw-justify-center tw-items-center" >
+    <swiper
+    :effect="'cards'"
+    :grabCursor="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>HELLO BABY KO</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+  </div>
   </div>
 
-  <Swiper
-    class="swiper-cards"
-    :width="240"
-    :modules="[SwiperAutoplay, SwiperEffectCards]"
-    :slides-per-view="1"
-    :loop="true"
-    :effect="'cards'"
-    :autoplay="{
-      delay: 8000,
-      disableOnInteraction: true
-    }"
-  >
-    <SwiperSlide
-      v-for="(slide, idx) in slides"
-      :key="idx"
-      :style="`background-color: ${slide.bg}; color: ${slide.color}`"
-    >
-      {{ idx }}
-    </SwiperSlide>
-  </Swiper>
 </template>
 
+
+
 <style scoped>
+.swiper {
+  width: 320px;
+  height: 320px;
+}
+
 .swiper-slide {
   display: flex;
-  justify-content: center;
   align-items: center;
-  font-size: 18px;
-  height: 20vh;
-  font-size: 4rem;
+  justify-content: center;
+  border-radius: 18px;
+  font-size: 22px;
   font-weight: bold;
-  font-family: 'Roboto', sans-serif;
+  color: #fff;
 }
-.swiper-wrapper {
-  min-width: 100vh;
-  width: 100vh;
+
+.swiper-slide:nth-child(1n) {
+  background-color: rgb(206, 17, 17);
 }
-.swiper-cards {
-  width: 240px;
-  height: 240px;
+
+.swiper-slide:nth-child(2n) {
+  background-color: rgb(0, 140, 255);
 }
-.swiper-cards .swiper-slide {
-  border-radius: 6px;
-  border: 1px solid black;
+
+.swiper-slide:nth-child(3n) {
+  background-color: rgb(10, 184, 111);
+}
+
+.swiper-slide:nth-child(4n) {
+  background-color: rgb(211, 122, 7);
+}
+
+.swiper-slide:nth-child(5n) {
+  background-color: rgb(118, 163, 12);
+}
+
+.swiper-slide:nth-child(6n) {
+  background-color: rgb(180, 10, 47);
+}
+
+.swiper-slide:nth-child(7n) {
+  background-color: rgb(35, 99, 19);
+}
+
+.swiper-slide:nth-child(8n) {
+  background-color: rgb(0, 68, 255);
+}
+
+.swiper-slide:nth-child(9n) {
+  background-color: rgb(218, 12, 218);
+}
+
+.swiper-slide:nth-child(10n) {
+  background-color: rgb(54, 94, 77);
 }
 </style>
